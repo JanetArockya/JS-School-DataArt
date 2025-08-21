3f5214995e41d7d36f8d48a91edac948fe3f9122
Janet Arockya <139042890+JanetArockya@users.noreply.github.com>
2025-08-21 18:41:02 +0530

    Delete dist directory

--- dist/modal.js
+++
@@ -1,7 +0,0 @@
-export function openModal(content) {
-    const modal = document.getElementById("modal");
-    if (!modal)
-        return;
-    modal.innerHTML = `<div class="modal-content">${content}</div>`;
-    modal.style.display = "block";
-}
